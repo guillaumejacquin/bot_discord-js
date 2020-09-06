f94cd93d5dc0d857e788ed6334e0e3f023cdb90d
Sobihan <sobihan.mahedran@epitech.eu>
2020-09-06 14:03:59 +0200

    [Added] Gif random

--- index.js
+++ index.js
@@ -1,47 +1,35 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
+const GphApiClient = require("giphy-js-sdk-core");
+const giphy = GphApiClient("fFgr63OWgA5JJnG7Xd7dd0TowZrpcykQ");
 
 bot.on('ready', function () {
     console.log("Je suis connecté !")
 })
 
+
 bot.on('message', message => {
-    if (message.content === 'ping') {
-        // message.reply('pong !')
-        message.channel.send('pong')
+    if (message.content === 'slap') {
+        giphy.search("gifs", { q: "slap" }).then(response => {
+        var totalResponse = response.data.length;
+        var responseIndex = Math.floor(Math.random() * 10 + 1) % totalResponse;
+        var responseFinal = response.data[responseIndex];
+        message.channel.send(
+        {
+        files: [responseFinal.images.fixed_height.url]
+        }
+        );
+    })
+    }
+    if(message.content === 'ping') {
+        console.log("Je recois 1 mesg")
+
+        message.channel.send("Pinging...").then(m =>{
+            var ping = m.createdTimestamp - message.createdTimestamp;
+            var botPing = Math.round(bot.pi);
+
+            m.edit(`**:ping_pong: Pong! Your Ping Is:-**\n  ${ping}ms`);
+        });
     }
 })
-
-
-bot.on('message', message => {
-
-    if (message.content.startsWith('!play')) {
-        // On récupère le premier channel audio du serveur
-        let voiceChannel = message.guild.channels
-            .filter(function (channel) { return channel.type === 'voice' })
-            .first()
-        // On récupère les arguments de la commande 
-        // il faudrait utiliser une expression régulière pour valider le lien youtube
-        let args = message.content.split(' ')
-        // On rejoint le channel audio
-        voiceChannel
-            .join()
-            .then(function (connection) {
-                // On démarre un stream à partir de la vidéo youtube
-                let stream = YoutubeStream(args[1])
-                stream.on('error', function () {
-                    message.reply("Je n'ai pas réussi à lire cette vidéo :(")
-                    connection.disconnect()
-                })
-                // On envoie le stream au channel audio
-                // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
-                connection
-                    .playStream(stream)
-                    .on('end', function () {
-                        connection.disconnect()
-                    })
-            })
-    }
-
-    bot.login("NzQ3NDc4MTU2NDc0MTg3OTA2.X0PdRw.TSjwQ2eJD2tjLlH_tMUDL7yKv0U")
-
+bot.login("NzUyMDgwMDEyNzk0MDAzNTI5.X1SbGA.ABgJnR8F5ql5X_swC00tRtiFb9s")
