f8bb92a4a41f4a59e54d43de55b6abbc6aff3b6e
Sobihan <sobihan.mahedran@epitech.eu>
2020-09-07 20:36:30 +0200

    [Added] A new random algorithm

--- index.js
+++ index.js
@@ -8,26 +8,23 @@ bot.on('ready', function () {
     console.log("Je suis connecté !")
 })
 
-
 bot.on('message', message => {
-    if (message.content === 'slap') {
-        giphy.search("gifs", { q: "slap" }).then(response => {
-        var totalResponse = response.data.length;
-        var responseIndex = Math.floor(Math.random() * 10 + 1) % totalResponse;
-        var responseFinal = response.data[responseIndex];
-        message.channel.send(
-        {
-        files: [responseFinal.images.fixed_height.url]
-        }
-        );
-    })
+    if (message.content == 'slap') {
+        giphy.random('gifs', {tag: 'slap'})
+            .then((response) => {
+                message.channel.send(
+                    {
+                        files: [response["data"]["images"]["original"]["gif_url"]]
+                    }
+                )
+            })
+            .catch((err) => {
+
+            })
     }
     if(message.content === 'ping') {
-        console.log("Je recois 1 mesg")
-
         message.channel.send("Pinging...").then(m =>{
             var ping = m.createdTimestamp - message.createdTimestamp;
-            var botPing = Math.round(bot.pi);
 
             m.edit(`**:ping_pong: Pong! Your Ping Is:-**\n  ${ping}ms`);
         });
